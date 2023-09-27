0ac8ce878281d0e16147cd171520c90755520cdc
David Waite <daveycrockett02@gmail.com>
2023-09-27 13:32:34 -0600

    eslint fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
               </ShowProjectButton>
               <ShowProjectButton name="Field Session" imageHref="undraw_medicine_b-1-ol.svg">
                 <p>
-                  I worked on a software team on behalf of Cobalt, Inc., where I helped to create the front-end of a medical test designed to improve a patient's cognitive ability and mental health. This included taking raw data and transforming it into numbers that could be displayed using bar graphs and spider charts. The graphs were responsive to the data being received at any moment, and also synchronized with different frequencies of sound for those who are hearing impaired. I also connected the software to hardware API and tested it using synthetic data. The project was done mainly in .NET C# and XAML, with the exception of some Python.
+                  {" I worked on a software team on behalf of Cobalt, Inc., where I helped to create the front-end of a medical test designed to improve a patient's cognitive ability and mental health. This included taking raw data and transforming it into numbers that could be displayed using bar graphs and spider charts. The graphs were responsive to the data being received at any moment, and also synchronized with different frequencies of sound for those who are hearing impaired. I also connected the software to hardware API and tested it using synthetic data. The project was done mainly in .NET C# and XAML, with the exception of some Python. "}
                 </p>
               </ShowProjectButton>
             </ul>
